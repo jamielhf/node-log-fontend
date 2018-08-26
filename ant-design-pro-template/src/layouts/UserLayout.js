cd6e01919d40016cf0d7b866b7c9224c77933893
linhaifeng <569309786@qq.com>
2018-08-26 17:24:19 +0800

    删除一些模块

--- ant-design-pro-template/src/layouts/UserLayout.js
+++ ant-design-pro-template/src/layouts/UserLayout.js
@@ -1,10 +1,9 @@
 import React, { Fragment } from 'react';
-import { Link, Redirect, Switch, Route } from 'dva/router';
+import { Redirect, Switch, Route } from 'dva/router';
 import DocumentTitle from 'react-document-title';
 import { Icon } from 'antd';
 import GlobalFooter from '../components/GlobalFooter';
 import styles from './UserLayout.less';
-import logo from '../assets/logo.svg';
 import { getRoutes, getPageQuery, getQueryPath } from '../utils/utils';
 
 const links = [
@@ -56,15 +55,6 @@ class UserLayout extends React.PureComponent {
       <DocumentTitle title={this.getPageTitle()}>
         <div className={styles.container}>
           <div className={styles.content}>
-            <div className={styles.top}>
-              <div className={styles.header}>
-                <Link to="/">
-                  <img alt="logo" className={styles.logo} src={logo} />
-                  <span className={styles.title}>Ant Design</span>
-                </Link>
-              </div>
-              <div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>
-            </div>
             <Switch>
               {getRoutes(match.path, routerData).map(item => (
                 <Route
