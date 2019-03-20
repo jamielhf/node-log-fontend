ea1777dd4acf376a66a6a63ce0ac25e96d2781cd
linhaifeng <linhaifeng@yy.com>
2019-03-20 15:47:12 +0800

    chore : 重构代码目录

---
+++ src/pages/Index/index.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { connect } from 'dva';
+import { Link} from 'dva/router';
+import Button from 'antd/lib/button';
+import './index.css';
+
+class Index extends React.Component<any>{
+  public add() {
+    this.props.dispatch({
+      type: 'global/add',
+    })
+  }
+  public continue() {
+    this.props.dispatch({
+      type: 'global/continue',
+    })
+  }
+  public render() {
+    console.log(this.props);
+    return (
+      <div className='App'>
+      <div>
+      <Link to='add'>go to add</Link>
+      </div>
+         <p>
+         counter {this.props.global.counter}
+         </p>
+         <Button>123</Button>
+         <button onClick={ ()=>this.add() }>add</button>
+         <button onClick={ ()=>this.continue() }>continue</button>
+      </div>
+    );
+  }
+}
+
+export default connect(({ global }: any) => ({
+  global,
+}))(Index);
--- typings.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'dva';
-
-declare module '*.css';
-
-
-declare module '*.jpg';
